901536633a1749b62b7cba75deb9943237c0d356
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-04-06 15:47:17 +0500

    fix: wrong name in import feat: migrations

--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
-from catalog import models as catalog_models
+from catalog import models
 
 
 def home(request):
     template = "homepage/home.html"
-    items = catalog_models.Item.objects.filter(is_published=True).order_by("?")[:3]
+    items = models.Item.objects.filter(is_published=True).order_by("?")[:3]
     context = {"items": items}
 
     return render(request, template, context)
