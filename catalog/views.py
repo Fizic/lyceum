874a67a7a7aafa63f7441ad77b764846120a4a58
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-04-07 12:07:04 +0500

    fix: imports, querysets, html templates, usage get_object_or_404

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,14 @@
+from django.db.models import Prefetch
 from django.http import Http404
-from django.shortcuts import render
-from . import models
+from django.shortcuts import render, get_object_or_404
+from catalog import models
 from django.core.exceptions import ObjectDoesNotExist
 
 
 def item_list(request):
     template = "catalog/item_list.html"
-    items = models.Item.objects.prefetch_related("tags").order_by("?").filter(is_published=True)
+    items = models.Item.objects.prefetch_related(
+        Prefetch("tags", queryset=models.Tag.objects.filter(is_published=True))).order_by("?").filter(is_published=True)
     context = {"items": items}
 
     return render(request, template, context)
@@ -14,7 +16,9 @@ def item_list(request):
 
 def item_detail(request, pk: int):
     template = "catalog/item_detail.html"
-    item = models.Item.objects.prefetch_related("tags").get_object_or_404(id=pk)
+    item = get_object_or_404(
+        models.Item.objects.prefetch_related(Prefetch("tags", queryset=models.Tag.objects.filter(is_published=True))),
+        id=pk)
     context = {"item": item}
 
     return render(request, template, context)
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 
 from catalog import models
@@ -5,7 +6,7 @@ from catalog import models
 
 def home(request):
     template = "homepage/home.html"
-    items = models.Item.objects.filter(is_published=True).order_by("?")[:3]
+    items = models.Item.objects.prefetch_related(Prefetch("tags", queryset=models.Tag.objects.filter(is_published=True))).filter(is_published=True).order_by("?")[:3]
     context = {"items": items}
 
     return render(request, template, context)
