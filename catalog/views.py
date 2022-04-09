c2aa0b37efc35fc4d321de34329e7acccfbd7ffa
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-04-09 12:53:49 +0500

    fix: item_detail

--- catalog/views.py
+++ catalog/views.py
@@ -14,8 +14,12 @@ def item_list(request):
 def item_detail(request, pk: int):
     template = "catalog/item_detail.html"
     item = get_object_or_404(
-        Item.objects.prefetch_related(Prefetch("tags", queryset=Tag.objects.filter(is_published=True))).only("tags"),
-        id=pk)
+        Item.objects.prefetch_related(
+            Prefetch("tags", queryset=Tag.objects.filter(is_published=True))
+        ).select_related(
+            "category"
+        ).only("name", "text", "category__name", "tags__name"), id=pk
+    )
     context = {"item": item}
 
     return render(request, template, context)
