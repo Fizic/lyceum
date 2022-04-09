2ee63040cf13c35d075b6743b52324d3d260e51a
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-04-09 12:50:25 +0500

    fix: imports, only use

--- catalog/views.py
+++ catalog/views.py
@@ -6,7 +6,9 @@ from catalog.models import Tag, Item
 def item_list(request):
     template = "catalog/item_list.html"
     items = Item.objects.prefetch_related(
-        Prefetch("tags", queryset=Tag.objects.filter(is_published=True))).order_by("?").filter(is_published=True).only("tags")
+        Prefetch("tags", queryset=Tag.objects.filter(is_published=True))).order_by("?").filter(is_published=True).only(
+        "name", "text", "tags__name"
+    )
     context = {"items": items}
 
     return render(request, template, context)
@@ -15,8 +17,12 @@ def item_list(request):
 def item_detail(request, pk: int):
     template = "catalog/item_detail.html"
     item = get_object_or_404(
-        Item.objects.prefetch_related(Prefetch("tags", queryset=Tag.objects.filter(is_published=True))).only("tags"),
-        id=pk)
+        Item.objects.prefetch_related(
+            Prefetch("tags", queryset=Tag.objects.filter(is_published=True))
+        ).select_related(
+            "category"
+        ).only("name", "text", "category__name", "tags__name"), id=pk
+    )
     context = {"item": item}
 
     return render(request, template, context)
