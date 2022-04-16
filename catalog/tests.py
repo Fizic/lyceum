f2ceaf59e6c62ad1803471c4fe16ee998d3de721
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-04-16 09:16:35 +0500

    fix: tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -13,19 +13,19 @@ class ItemDetailTest(TestCase):
 
     def test_item_info(self):
         csrf_client = Client(enforce_csrf_checks=True)
-        response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1))
+        response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1.id))
         self.assertEqual(response.context["item"].name, "test")
         self.assertEqual(response.context["item"].text, "text роскошно")
 
     def test_item_detail_unauthorized_view(self):
         csrf_client = Client(enforce_csrf_checks=True)
-        response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1))
+        response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1.id))
         self.assertEqual(response.context["user"].username, "")
 
     def test_item_detail_authorized_view_without_rating(self):
         csrf_client = Client(enforce_csrf_checks=True)
         csrf_client.login(username="admin", password="admin")
-        response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1))
+        response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1.id))
         self.assertEqual(response.context["user"].username, "admin")
 
     def test_item_set_rating(self):
@@ -34,5 +34,5 @@ class ItemDetailTest(TestCase):
         """
         client = Client()
         client.login(username="admin", password="admin")
-        response = client.post("/catalog/{pk}/".format(pk=self.test_item_1), data={"rating": ["6"]})
+        response = client.post("/catalog/{pk}/".format(pk=self.test_item_1.id), data={"rating": ["6"]})
         self.assertEqual(response.content, b"Rating must be between 0 and 5")
