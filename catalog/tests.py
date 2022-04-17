0465c8192ac5eaf793ab99897ed9b2580c8a2c8d
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-04-17 14:31:24 +0500

    fix: tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,5 @@
+import random
+import string
 from django.contrib.auth.models import User
 from django.test import Client
 
@@ -9,24 +11,31 @@ from catalog.models import Item
 class ItemDetailTest(TestCase):
     def setUp(self):
         self.test_item_1 = Item.objects.create(name="test", text="text роскошно")
-        User.objects.create_user(username="admin", password="admin")
+        self.test_user_1 = User.objects.create_user(username="admin", password="admin")
 
     def test_item_info(self):
         csrf_client = Client(enforce_csrf_checks=True)
         response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1.id))
-        self.assertEqual(response.context["item"].name, "test")
-        self.assertEqual(response.context["item"].text, "text роскошно")
+        self.assertEqual(response.context["item"], self.test_item_1)
 
-    def test_item_detail_unauthorized_view(self):
+    def item_detail_unauthorized_view(self):
         csrf_client = Client(enforce_csrf_checks=True)
         response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1.id))
         self.assertEqual(response.context["user"].username, "")
 
-    def test_item_detail_authorized_view_without_rating(self):
+    def item_detail_authorized_view_without_rating(self, username: str, password: str, test_user: User):
         csrf_client = Client(enforce_csrf_checks=True)
-        csrf_client.login(username="admin", password="admin")
+        csrf_client.login(username=username, password=password)
         response = csrf_client.get("/catalog/{pk}/".format(pk=self.test_item_1.id))
-        self.assertEqual(response.context["user"].username, "admin")
+        self.assertEqual(response.context["user"], test_user)
+
+    def test_item_detail(self):
+        for _ in range(10):
+            username = ''.join(random.choices(string.ascii_letters, k=10))
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+            test_user = User.objects.create_user(username=username, password=password)
+            self.item_detail_authorized_view_without_rating(username, password, test_user)
+            self.item_detail_unauthorized_view()
 
     def test_item_set_rating(self):
         """
